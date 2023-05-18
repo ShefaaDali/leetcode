a9392d0d8166679ff5a104b06673dfc86f2880f3
Shefaa Dali <70350753+ShefaaDali@users.noreply.github.com>
2023-05-18 19:18:43 +0300

    Update 0217-contains-duplicate.java | add another solution

--- java/0217-contains-duplicate.java
+++ java/0217-contains-duplicate.java
@@ -1,5 +1,5 @@
 class Solution {
-
+  //O(n)
     public boolean containsDuplicate(int[] nums) {
         Set<Integer> uniques = new HashSet<>();
         for (int i = 0; i < nums.length; i++) {
@@ -9,5 +9,11 @@ class Solution {
             uniques.add(nums[i]);
         }
         return false;
+        //Solution 2 | O(n log n)
+         /* Arrays.sort(nums);   
+        for(int i=0;i<nums.length-1;i++)
+        if (nums[i]==nums[i+1])
+        return true;
+        return false;*/
     }
 }
