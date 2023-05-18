8408917243de98a8f650471ed33c5685b8a9f493
Shefaa Dali <70350753+ShefaaDali@users.noreply.github.com>
2023-05-18 19:07:35 +0300

    Update 0242-valid-anagram.java | add 3 other solutions

--- java/0242-valid-anagram.java
+++ java/0242-valid-anagram.java
@@ -1,5 +1,5 @@
 class Solution {
-
+    //time complexity of O(n)
     public boolean isAnagram(String s, String t) {
         if (s.length() != t.length()) return false;
 
@@ -13,5 +13,38 @@ class Solution {
         for (int n : store) if (n != 0) return false;
 
         return true;
+          
+        
+        //Solution 2 |time complexity of O(n log n)
+        /* if(s.length()!=t.length()) return false;
+        char[] sArray = s.toCharArray();
+        Arrays.sort(sArray);
+        char[] tArray = t.toCharArray();
+        Arrays.sort(tArray);
+        for(int i=0;i<tArray.length;i++)
+        if(sArray[i]!=tArray[i])
+        return false;
+        return true;*/
+        
+        //Solution 3 |time complexity of O(n log n)
+        /*
+         if(s.length()!=t.length()) return false;
+        char[] sArray = s.toCharArray();
+        Arrays.sort(sArray);
+        char[] tArray = t.toCharArray();
+        Arrays.sort(tArray);
+        s = new String(sArray);
+        t = new String(tArray);
+        if(s.equals(t))return true;
+        return false;/*
+        
+        
+       //Solution 4 |time complexity of O(n)
+        /*public boolean isAnagram(String s, String t) {
+	Map<Integer, Integer> freqs = new HashMap<>();
+	s.chars().forEach(ch -> freqs.put(ch, freqs.getOrDefault(ch, 0) + 1));
+	t.chars().forEach(ch -> freqs.put(ch, freqs.getOrDefault(ch, 0) - 1));
+	return freqs.values().stream().allMatch(x -> x == 0);*/
+}	
     }
 }
