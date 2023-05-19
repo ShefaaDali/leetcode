93944ce802b59760b87d5a1914c08d8b53e598b0
Shefaa Dali <70350753+ShefaaDali@users.noreply.github.com>
2023-05-19 14:30:12 +0300

    Update 0001-two-sum.java | add solution

--- java/0001-two-sum.java
+++ java/0001-two-sum.java
@@ -1,5 +1,5 @@
 class Solution {
-
+//O(n)
     public int[] twoSum(int[] nums, int target) {
         HashMap<Integer, Integer> prevMap = new HashMap<>();
 
@@ -15,5 +15,17 @@ class Solution {
         }
 
         return new int[] {};
+        //solution 2 |time complexity O(n^2)
+        /*
+        int[] ans=new int [2];
+        for (int i=0;i<nums.length-1;i++){
+            for (int j=i+1;j<nums.length;j++)
+            if(nums[i]+nums[j]==target){
+               ans[0] =i;
+                ans[1]=j;
+                break;
+            }
+        }
+        return ans;*/
     }
 }
