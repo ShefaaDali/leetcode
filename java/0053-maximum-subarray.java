d855e7ec9fc4714ac9aaf3ea45435daaf83697e6
Shefaa Dali <70350753+ShefaaDali@users.noreply.github.com>
2023-06-08 14:46:11 +0300

    Update 0053-maximum-subarray.java | another solution

--- java/0053-maximum-subarray.java
+++ java/0053-maximum-subarray.java
@@ -17,3 +17,24 @@ class Solution {
         return max;
     }
 }
+
+//solustion 2
+class Solution {
+    public int maxSubArray(int[] nums) {
+        if(nums.length==1) return nums[0];
+        int left=0;
+        int right=1;
+        int sum=nums[0];
+        int max=nums[left];
+        while(right!=nums.length){
+            sum+=nums[right];       
+            if(nums[right]>sum){
+                left = right;
+                sum=nums[left]; 
+            }
+            if(sum>max) max=sum;    
+             right++;
+            }
+                return max;
+    }
+}
